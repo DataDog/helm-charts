50e597e03a594860dcba80fd03dacdd01f4ee7d8
cedric lamoriniere <cedric.lamoriniere@datadoghq.com>
2020-11-05 23:33:29 +0100

    Add v1beta1 and v1

--- charts/datadog-crds/update-crds.sh
+++ charts/datadog-crds/update-crds.sh
@@ -9,21 +9,31 @@ if [[ $# -eq 1 ]] ; then
 fi
 
 download_crd() {
-    file=datadoghq.com_$2.yaml
-    path=$ROOT/charts/datadog-crds/templates/$file
-    echo "Download CRD \"$file\" from tag \"$1\""
-    curl --silent --show-error --fail --location --output $path https://raw.githubusercontent.com/DataDog/datadog-operator/$1/bundle/manifests/$file
+    inFile=datadoghq.com_$2_crd.yaml
+    version=$4
+    outFile=datadoghq.com_$2_$version.yaml
+    path=$ROOT/charts/datadog-crds/templates/$outFile
+    echo "Download CRD \"$inFile\" version \"$version\" from tag \"$1\""
+    curl --silent --show-error --fail --location --output $path https://raw.githubusercontent.com/DataDog/datadog-operator/$1/deploy/crds/$version/$inFile
 
-    cp $path $ROOT/crds/$file
+    ifCondition="{{- if and .Values.crds.$3 (not (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\")) }}"
+    if [ "$version" = "v1" ]; then
+        ifCondition="{{- if and .Values.crds.$3 (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\") }}"
+        cp $path $ROOT/crds/datadoghq.com_$2.yaml
+    fi
+    
     yq w -i $path 'metadata.labels."helm.sh/chart"' '{{ include "datadog-crds.chart" . }}'
     yq w -i $path 'metadata.labels."app.kubernetes.io/managed-by"' '{{ .Release.Service }}'
     yq w -i $path 'metadata.labels."app.kubernetes.io/name"' '{{ include "datadog-crds.name" . }}'
     yq w -i $path 'metadata.labels."app.kubernetes.io/instance"' '{{ .Release.Name }}'
-    { echo "{{- if .Values.crds.$3 }}"; cat $path; } > tmp.file
+    
+    { echo "$ifCondition"; cat $path; } > tmp.file
     mv tmp.file $path
     echo '{{- end }}' >> $path
 }
 
 mkdir -p $ROOT/crds
-download_crd $TAG datadogmetrics datadogMetrics
-download_crd $TAG datadogagents datadogAgents
+download_crd $TAG datadogmetrics datadogMetrics v1beta1
+download_crd $TAG datadogmetrics datadogMetrics v1
+download_crd $TAG datadogagents datadogAgents v1beta1
+download_crd $TAG datadogagents datadogAgents v1
