84dd9cb2a4486ff93cf5990ea6bf859066d123ab
Lénaïc Huard <lenaic.huard@datadoghq.com>
2021-01-29 09:36:44 +0100

    Update the `update-crds.sh` script for `yq` 4
    * `yq` 4 isn’t compatible with `yq` 3 and `brew` now installs the 4;
    * Fix `shellcheck` warnings.

--- charts/datadog-crds/update-crds.sh
+++ charts/datadog-crds/update-crds.sh
@@ -14,26 +14,28 @@ download_crd() {
     outFile=datadoghq.com_$2_$version.yaml
     path=$ROOT/charts/datadog-crds/templates/$outFile
     echo "Download CRD \"$inFile\" version \"$version\" from tag \"$1\""
-    curl --silent --show-error --fail --location --output $path https://raw.githubusercontent.com/DataDog/datadog-operator/$1/config/crd/bases/$version/$inFile
+    curl --silent --show-error --fail --location --output "$path" "https://raw.githubusercontent.com/DataDog/datadog-operator/$1/config/crd/bases/$version/$inFile"
 
     ifCondition="{{- if and .Values.crds.$3 (not (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\")) }}"
     if [ "$version" = "v1" ]; then
         ifCondition="{{- if and .Values.crds.$3 (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\") }}"
-        cp $path $ROOT/crds/datadoghq.com_$2.yaml
+        cp "$path" "$ROOT/crds/datadoghq.com_$2.yaml"
     fi
-    
-    yq w -i $path 'metadata.labels."helm.sh/chart"' '{{ include "datadog-crds.chart" . }}'
-    yq w -i $path 'metadata.labels."app.kubernetes.io/managed-by"' '{{ .Release.Service }}'
-    yq w -i $path 'metadata.labels."app.kubernetes.io/name"' '{{ include "datadog-crds.name" . }}'
-    yq w -i $path 'metadata.labels."app.kubernetes.io/instance"' '{{ .Release.Name }}'
-    
-    { echo "$ifCondition"; cat $path; } > tmp.file
-    mv tmp.file $path
-    echo '{{- end }}' >> $path
+
+    VALUE="'{{ include \"datadog-crds.chart\" . }}'" \
+    yq eval '.metadata.labels."helm.sh/chart" = env(VALUE)'                              -i "$path"
+    yq eval '.metadata.labels."app.kubernetes.io/managed-by" = "{{ .Release.Service }}"' -i "$path"
+    VALUE="'{{ include \"datadog-crds.name\" . }}'" \
+    yq eval '.metadata.labels."app.kubernetes.io/name" = env(VALUE)'                     -i "$path"
+    yq eval '.metadata.labels."app.kubernetes.io/instance" = "{{ .Release.Name }}"'      -i "$path"
+
+    { echo "$ifCondition"; cat "$path"; } > tmp.file
+    mv tmp.file "$path"
+    echo '{{- end }}' >> "$path"
 }
 
-mkdir -p $ROOT/crds
-download_crd $TAG datadogmetrics datadogMetrics v1beta1
-download_crd $TAG datadogmetrics datadogMetrics v1
-download_crd $TAG datadogagents datadogAgents v1beta1
-download_crd $TAG datadogagents datadogAgents v1
+mkdir -p "$ROOT/crds"
+download_crd "$TAG" datadogmetrics datadogMetrics v1beta1
+download_crd "$TAG" datadogmetrics datadogMetrics v1
+download_crd "$TAG" datadogagents datadogAgents v1beta1
+download_crd "$TAG" datadogagents datadogAgents v1
