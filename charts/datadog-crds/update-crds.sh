c5280b702acdccd3c044d36180a32b52a0080e12
David Ortiz <david.ortiz@datadoghq.com>
2021-08-02 13:12:38 +0200

    datadog-crds/update-crds: adapt to download CRDs from the extendeddaemonset repo

--- charts/datadog-crds/update-crds.sh
+++ charts/datadog-crds/update-crds.sh
@@ -3,23 +3,32 @@ set -euo pipefail
 
 ROOT=$(git rev-parse --show-toplevel)
 
-TAG=master
-if [[ $# -eq 1 ]] ; then
-    TAG=$1
+DATADOG_OPERATOR_REPO=Datadog/datadog-operator
+DATADOG_EXTENDED_DAEMON_SET_REPO=Datadog/extendeddaemonset
+
+DATADOG_OPERATOR_TAG=master
+if [[ $# -eq 1 ]] || [[ $# -eq 2 ]]; then
+    DATADOG_OPERATOR_TAG=$1
+fi
+
+DATADOG_EXTENDED_DAEMON_SET_TAG=master
+if [[ $# -eq 2 ]] ; then
+   DATADOG_EXTENDED_DAEMON_SET_TAG=$2
 fi
 
 download_crd() {
-    tag=$1
-    name=$2
-    camelCaseName=$3
-    version=$4
+    repo=$1
+    tag=$2
+    name=$3
+    camelCaseName=$4
+    version=$5
 
     inFile=datadoghq.com_$name.yaml
     # shellcheck disable=SC2154
     outFile=datadoghq.com_"$name"_"$version".yaml
     path=$ROOT/charts/datadog-crds/templates/$outFile
-    echo "Download CRD \"$inFile\" version \"$version\" from tag \"$1\""
-    curl --silent --show-error --fail --location --output "$path" "https://raw.githubusercontent.com/DataDog/datadog-operator/$tag/config/crd/bases/$version/$inFile"
+    echo "Download CRD \"$inFile\" version \"$version\" from repo \"$repo\" tag \"$tag\""
+    curl --silent --show-error --fail --location --output "$path" "https://raw.githubusercontent.com/$repo/$tag/config/crd/bases/$version/$inFile"
 
     ifCondition="{{- if and .Values.crds.$camelCaseName (not (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\")) }}"
     if [ "$version" = "v1" ]; then
@@ -40,9 +49,16 @@ download_crd() {
 }
 
 mkdir -p "$ROOT/crds"
-download_crd "$TAG" datadogmetrics datadogMetrics v1beta1
-download_crd "$TAG" datadogmetrics datadogMetrics v1
-download_crd "$TAG" datadogagents datadogAgents v1beta1
-download_crd "$TAG" datadogagents datadogAgents v1
-download_crd "$TAG" datadogmonitors datadogMonitors v1beta1
-download_crd "$TAG" datadogmonitors datadogMonitors v1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmetrics datadogMetrics v1beta1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmetrics datadogMetrics v1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogagents datadogAgents v1beta1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogagents datadogAgents v1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmonitors datadogMonitors v1beta1
+download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmonitors datadogMonitors v1
+
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetreplicasets extendedDaemonSetReplicaSets v1beta1
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetreplicasets extendedDaemonSetReplicaSets v1
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsets extendedDaemonSets v1beta1
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsets extendedDaemonSets v1
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetsettings extendedDaemonSettings v1beta1
+download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetsettings extendedDaemonSettings v1
