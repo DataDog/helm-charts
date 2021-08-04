6a11c9ca7a2a8b547cbf6f13c57f05532c3ca284
David Ortiz <david.ortiz@datadoghq.com>
2021-08-04 10:46:00 +0200

    [datadog-crds] Fix script to download correct EDS CRDs (#334)
    * datadog-crds/update-crds: download only v1beta1 for EDS
    
    * datadog-crds/values: add option to enable EDS CRDs
    
    * crds: regenerate EDS CRDs and templates
    
    * datadog-crds: bump version
    
    * datadog-crds: update docs with script
    
    * datadog-crds/update-crds: rename camelCaseName for clarity

--- charts/datadog-crds/update-crds.sh
+++ charts/datadog-crds/update-crds.sh
@@ -20,7 +20,7 @@ download_crd() {
     repo=$1
     tag=$2
     name=$3
-    camelCaseName=$4
+    installOption=$4 # Name of the option to install the CRD (defined in values.yaml)
     version=$5
 
     inFile=datadoghq.com_$name.yaml
@@ -30,11 +30,21 @@ download_crd() {
     echo "Download CRD \"$inFile\" version \"$version\" from repo \"$repo\" tag \"$tag\""
     curl --silent --show-error --fail --location --output "$path" "https://raw.githubusercontent.com/$repo/$tag/config/crd/bases/$version/$inFile"
 
-    ifCondition="{{- if and .Values.crds.$camelCaseName (not (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\")) }}"
-    if [ "$version" = "v1" ]; then
-        ifCondition="{{- if and .Values.crds.$camelCaseName (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\") }}"
+    # This case is needed because v1 CRDs are not present in any released version of the EDS yet.
+    # Once they are, the EDS case should be handled as the operator is now.
+    case "$repo" in
+      "$DATADOG_OPERATOR_REPO")
+        ifCondition="{{- if and .Values.crds.$installOption (not (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\")) }}"
+        if [ "$version" = "v1" ]; then
+            ifCondition="{{- if and .Values.crds.$installOption (.Capabilities.APIVersions.Has \"apiextensions.k8s.io/v1/CustomResourceDefinition\") }}"
+            cp "$path" "$ROOT/crds/datadoghq.com_$name.yaml"
+        fi
+        ;;
+      "$DATADOG_EXTENDED_DAEMON_SET_REPO")
+        ifCondition="{{- if .Values.crds.$installOption }}"
         cp "$path" "$ROOT/crds/datadoghq.com_$name.yaml"
-    fi
+        ;;
+    esac
 
     VALUE="'{{ include \"datadog-crds.chart\" . }}'" \
     yq eval '.metadata.labels."helm.sh/chart" = env(VALUE)'                              -i "$path"
@@ -56,9 +66,9 @@ download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogagents data
 download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmonitors datadogMonitors v1beta1
 download_crd "$DATADOG_OPERATOR_REPO" "$DATADOG_OPERATOR_TAG" datadogmonitors datadogMonitors v1
 
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetreplicasets extendedDaemonSetReplicaSets v1beta1
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetreplicasets extendedDaemonSetReplicaSets v1
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsets extendedDaemonSets v1beta1
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsets extendedDaemonSets v1
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetsettings extendedDaemonSettings v1beta1
-download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" extendeddaemonsetsettings extendedDaemonSettings v1
+# v1 CRDs are not present in any released version yet. Add them here when they are.
+eds_crds=(extendeddaemonsetreplicasets extendeddaemonsets extendeddaemonsetsettings)
+for eds_crd in "${eds_crds[@]}"
+do
+  download_crd "$DATADOG_EXTENDED_DAEMON_SET_REPO" "$DATADOG_EXTENDED_DAEMON_SET_TAG" "$eds_crd" extendedDaemonSets v1beta1
+done
