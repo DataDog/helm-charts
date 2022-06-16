a8e9ad933ac090022e3b2f4058c121b62103ac1f
William Yardley <wyardley@users.noreply.github.com>
2022-06-15 21:23:32 -0700

    chore: update helm-docs version and support M1
    The existing script doesn't function properly for Apple M1 Macs.
    Use `uname -m` to get the arch, rather than hard-coding to x86_64.
    
    This also requires updating to a later version of helm-docs, so update
    from 1.5.0 to 1.10.0
    
    Use double quotes around the entire string with interpolated variables,
    vs. individual variables themselves.

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-HELM_DOCS_VERSION="1.5.0"
+HELM_DOCS_VERSION="1.10.0"
 OS=$(uname)
+ARCH=$(uname -m)
 
 # install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_${OS}_x86_64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz "https://github.com/norwoodj/helm-docs/releases/download/v${HELM_DOCS_VERSION}/helm-docs_${HELM_DOCS_VERSION}_${OS}_${ARCH}.tar.gz"
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
