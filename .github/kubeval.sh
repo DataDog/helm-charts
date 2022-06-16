94a1b612a574b8aa19e28fc0382b7e22d65ebe0e
William Yardley <wyardley@users.noreply.github.com>
2022-06-16 07:52:04 -0700

    ci: update schema location for chart validation
    Despite the note about checking for available schemas, many of the
    schemas being tested about do not exist at
    https://kubernetesjsonschema.dev/, which is no longer updated; this
    could result in a false sense of confidence (because of
    `--ignore-missing-schemas`, I believe this will fail soft)
    
    Update to later 1.18.x and 1.22.x variants in the matrix, add 1.24.x,
    and switch to the schemas at
    https://github.com/yannh/kubernetes-json-schema (used by `kubeconform`).

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 KUBEVAL_VERSION="v0.16.1"
-SCHEMA_LOCATION="https://kubernetesjsonschema.dev/"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master"
 OS=$(uname)
 
 CHANGED_CHARTS=${CHANGED_CHARTS:-${1:-}}
@@ -14,7 +14,7 @@ else
 fi
 
 # install kubeval
-curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-${OS}-amd64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz "https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-${OS}-amd64.tar.gz"
 tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
