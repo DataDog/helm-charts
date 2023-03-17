a8124de67d1100eb76535f474ac7fe88649723ac
Nate Schmidt <nathan.schmidt@motorefi.com>
2023-03-17 09:00:50 -0600

    feat(tr-76): adding custom ci to datadog-helm-charts
    Jira: https://motorefi.atlassian.net/browse/tr-76

---
+++ ci/scripts/run-helm-package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# boolean: indicating whether the operation will produce allure-results or not
+ALLURE_RESULTS=false
+
+# boolean: indicating whether or not the operation passed or failed (set to the exit code of the operation)
+# Set to fail by default (i.e. non-zero exit code)
+EXECUTION_PASSED=1
+
+# This function runs the tests.  It needs to set EXECUTION_PASSED to true if the tests pass or false otherwise.
+function run_package() {
+   helm package charts/datadog
+   EXECUTION_PASSED=$?
+}
+
+# Record the results for the parent workflow to read.
+function record_results() {
+   echo $EXECUTION_PASSED > /results/exit-code
+   echo $ALLURE_RESULTS > /results/allure-enabled
+}
+
+function main() {
+   run_package
+   record_results
+}
+
+main "$@"
