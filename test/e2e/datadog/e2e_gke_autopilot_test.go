2046734429d08111afc9c69a76fb77eb38bf2ad2
Fanny Jiang <fanny.jiang@datadoghq.com>
2025-05-16 15:48:09 -0400

    fix test

--- test/e2e/datadog/e2e_gke_autopilot_test.go
+++ test/e2e/datadog/e2e_gke_autopilot_test.go
@@ -13,8 +13,6 @@ import (
 	"github.com/DataDog/test-infra-definitions/components/datadog/kubernetesagentparams"
 	"github.com/DataDog/test-infra-definitions/scenarios/gcp/gke"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	gcpkubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners/gcp/kubernetes"
@@ -35,44 +33,28 @@ func TestGKEAutopilotSuite(t *testing.T) {
 		"ddinfra:env":                           auto.ConfigValue{Value: "gcp/agent-qa"},
 		"ddinfra:gcp/defaultPrivateKeyPassword": auto.ConfigValue{Value: gcpPrivateKeyPassword},
 	}
-	e2e.Run(t, &gkeAutopilotSuite{}, e2e.WithProvisioner(gcpkubernetes.GKEProvisioner(gcpkubernetes.WithGKEOptions(gke.WithAutopilot()), gcpkubernetes.WithAgentOptions(kubernetesagentparams.WithGKEAutopilot()), gcpkubernetes.WithExtraConfigParams(config))), e2e.WithSkipDeleteOnFailure(), e2e.WithDevMode())
+	e2e.Run(t, &gkeAutopilotSuite{}, e2e.WithProvisioner(gcpkubernetes.GKEProvisioner(gcpkubernetes.WithGKEOptions(gke.WithAutopilot()), gcpkubernetes.WithAgentOptions(kubernetesagentparams.WithGKEAutopilot()), gcpkubernetes.WithExtraConfigParams(config))), e2e.WithDevMode())
 }
 
 func (v *gkeAutopilotSuite) TestGKEAutopilot() {
 	v.T().Log("Running GKE test")
 	res, _ := v.Env().KubernetesCluster.Client().CoreV1().Pods("datadog").List(context.TODO(), v1.ListOptions{})
 
-	var agent corev1.Pod
 	containsAgent := false
 	for _, pod := range res.Items {
 		if strings.Contains(pod.Name, "agent") {
 			containsAgent = true
-			agent = pod
 			break
 		}
 	}
 	assert.True(v.T(), containsAgent, "Agent not found")
 
-	stdout, stderr, err := v.Env().KubernetesCluster.KubernetesClient.
-		PodExec("datadog", agent.Name, "agent", []string{"ls"})
-	require.NoError(v.T(), err)
-	assert.Empty(v.T(), stderr)
-	assert.NotEmpty(v.T(), stdout)
-
-	var clusterAgent corev1.Pod
 	containsClusterAgent := false
 	for _, pod := range res.Items {
 		if strings.Contains(pod.Name, "cluster-agent") {
 			containsClusterAgent = true
-			clusterAgent = pod
 			break
 		}
 	}
 	assert.True(v.T(), containsClusterAgent, "Cluster Agent not found")
-
-	stdout, stderr, err = v.Env().KubernetesCluster.KubernetesClient.
-		PodExec("datadog", clusterAgent.Name, "cluster-agent", []string{"ls"})
-	require.NoError(v.T(), err)
-	assert.Empty(v.T(), stderr)
-	assert.NotEmpty(v.T(), stdout)
 }
