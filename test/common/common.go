c35e28a7346c96162bfddd5644dfc34a53e29495
Fanny Jiang <fanny.jiang@datadoghq.com>
2026-01-23 12:05:37 -0500

    squash: yaml-mapper history (pre integ tests)

--- test/common/common.go
+++ test/common/common.go
@@ -90,6 +90,7 @@ func CreateSecretFromEnv(t *testing.T, kubectlOptions *k8s.KubectlOptions, apiKe
 
 	// Setup Datadog Agent
 	t.Log("Creating secret")
+	kubectlOptions.Logger = logger.Discard
 	k8s.RunKubectl(t, kubectlOptions, "create", "secret", "generic", "datadog-secret",
 		"--from-literal",
 		"api-key="+apiKey,
@@ -138,7 +139,7 @@ func Contains(str string, list []string) bool {
 	return false
 }
 
-// Takes multi-document YAML and filter out keys from each document.
+// FilterYamlKeysMultiManifest Takes multi-document YAML and filter out keys from each document.
 func FilterYamlKeysMultiManifest(manifest string, filterKeys map[string]interface{}) (string, error) {
 	reader := strings.NewReader(manifest)
 	decoder := yaml2.NewYAMLOrJSONDecoder(reader, 4096)
@@ -193,4 +194,35 @@ func filterKeysRecursive(yamlMap *map[string]interface{}, keys map[string]interf
 			filterKeysRecursive(&nested, keys)
 		}
 	}
+}
+
+func CurrentContext(t *testing.T) string {
+	val, err := k8s.RunKubectlAndGetOutputE(t, k8s.NewKubectlOptions("", "", ""), "config", "current-context")
+	require.Nil(t, err)
+	return val
+}
+
+func GetFullValues(t *testing.T, cmd HelmCommand, namespace string) string {
+	tempFile, err := os.CreateTemp("", "helm-values-*.yaml")
+	require.NoError(t, err, "failed to create temporary file")
+	defer tempFile.Close()
+
+	tempFilePath := tempFile.Name()
+
+	// Default namespace is datadog-agent
+	if namespace == "" {
+		namespace = "datadog-agent"
+	}
+	kubectlOptions := k8s.NewKubectlOptions("", "", namespace)
+	helmOptions := &helm.Options{
+		KubectlOptions: kubectlOptions,
+	}
+
+	output, err := helm.RunHelmCommandAndGetOutputE(t, helmOptions, "get", "values", cmd.ReleaseName, "--all")
+	require.NoError(t, err, "failed to get helm values")
+
+	err = os.WriteFile(tempFilePath, []byte(output), 0644)
+	require.NoError(t, err, "failed to write values to temporary file")
+
+	return tempFilePath
 }
