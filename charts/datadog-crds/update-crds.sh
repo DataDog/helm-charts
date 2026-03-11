160567fac92ea8ea310a77e6ea261146d71e3a2d
Volodymyr Starodubov <volodymyr.starodubov@alpacked.io>
2026-03-11 14:18:41 +0200

    chore: add `crdAnnotation` to helm charts
    Signed-off-by: Volodymyr Starodubov <volodymyr.starodubov@alpacked.io>

--- charts/datadog-crds/update-crds.sh
+++ charts/datadog-crds/update-crds.sh
@@ -44,8 +44,8 @@ download_crd() {
     mv tmp.file "$path"
     echo '{{- end }}' >> "$path"
 
-    # Add keepCrds annotation
-    sed -i.bak 's/^  annotations:$/  annotations:\n    {{- if .Values.keepCrds }}\n    helm.sh\/resource-policy: keep\n    {{- end }}/' "$path"
+    # Add keepCrds and crdAnnotations
+    sed -i.bak 's/^  annotations:$/  annotations:\n    {{- if .Values.keepCrds }}\n    helm.sh\/resource-policy: keep\n    {{- end }}\n    {{- with .Values.crdAnnotations }}\n    {{- toYaml . | nindent 4 }}\n    {{- end }}/' "$path"
     rm -f "$path.bak"
 }
 
